23346c6451dc4853976211e565cea906ad627e95
Thrupthi Shetty <thrupthishetty@gmail.com>
2018-03-02 19:07:36 +0000

    adding test cases GEt and post todos

---
+++ server/test/server.test.js
@@ -0,0 +1,95 @@
+const expect = require("expect")
+const request = require("supertest")
+
+const {app} = require("./../server")
+const {Todo} = require("./../models/todo")
+
+var todosArray = [{
+    text : "text 1here"
+},
+{
+    text : 'text 2 here'
+}]
+
+beforeEach((done) =>{
+    Todo.remove({}).then(() => {
+      Todo.insertMany(todosArray) ; 
+    }).then(() => done())
+})
+
+
+describe('POST/todos', () => {
+    
+    it('should create a new todo' , (done) =>{
+        var text = 'Test todo text';
+        
+        request(app)
+        .post('/todos')
+        .send({ text})
+        .expect(200)
+        .expect((res) =>{
+            expect(res.body.text).toBe(text);
+        }) 
+        .end((err,res) =>{
+            if(err){
+                return done(err);
+            }
+            
+            Todo.find({text}).then((todos) =>{
+                expect(todos.length).toBe(1)
+                expect(todos[0].text).toBe(text)
+                done();
+            }).catch((e) => done(e))
+        })
+    })
+   
+    it('should not create a new todo with invalid data' , (done) =>{
+      //  var text = 'Test todo text';
+        
+        request(app)
+        .post('/todos')
+        .send({})
+        .expect(400)
+         
+        .end((err,res) =>{
+            if(err){
+                return done(err);
+            }
+            
+            Todo.find().then((todos) =>{
+                expect(todos.length).toBe(2)
+                //expect(todos[0].text).toBe(text)
+                done();
+            }).catch((e) => done(e))
+        })
+    })
+    
+    
+   
+    
+})
+
+
+
+describe('GET /todos',() =>{
+    it('should get all todos' ,(done)=>{
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) =>{
+          expect(  res.body.todos.length).toBe(2)
+        }).end(done);
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
