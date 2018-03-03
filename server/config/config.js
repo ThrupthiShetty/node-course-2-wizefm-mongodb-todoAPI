b9bb273b06b3f15c209f85af2861639636ad3e1e
Thrupthi Shetty <thrupthishetty@gmail.com>
2018-03-03 18:47:32 +0000

    set up seperate test and devlopment env

---
+++ server/config/config.js
@@ -0,0 +1,13 @@
+var env = process.env.NODE_ENV || 'development'
+
+console.log("env *********",env)
+
+if(env === 'development'){
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI =  'mongodb://wizetrndbuser:welcome1@localhost:27017/wizetrndb';
+    
+}else if(env ==='test'){
+     process.env.PORT = 3000;
+    process.env.MONGODB_URI =  'mongodb://wizetrndbuser:welcome1@localhost:27017/wizetrndbtest';
+    
+}
